c5eb8f731b8827b1d26e5c355278bca6382595d7
Serhii Marchenko <serhii.marchenko@Kaltura.com>
2021-06-01 11:05:37 +0300

    tiny changes after style-review with Dana

--- src/components/side-by-side/side-by-side.tsx
+++ src/components/side-by-side/side-by-side.tsx
@@ -10,12 +10,14 @@ interface SideBySideComponentOwnProps {
 }
 interface SideBySideComponentConnectProps {
   guiClientRect?: {width: number; height: number};
+  playerHover?: boolean;
 }
 
 type SideBySideComponentProps = SideBySideComponentOwnProps & SideBySideComponentConnectProps;
 
 const mapStateToProps = (state: Record<string, any>) => ({
-  guiClientRect: state.shell.guiClientRect
+  guiClientRect: state.shell.guiClientRect,
+  playerHover: state.shell.playerHover
 });
 @connect(mapStateToProps)
 export class SideBySide extends Component<SideBySideComponentProps> {
@@ -25,17 +27,27 @@ export class SideBySide extends Component<SideBySideComponentProps> {
     this.ref.current.appendChild(this.props.secondaryPlayer.getView());
   }
 
-  render({guiClientRect, onPIPSwitch}: SideBySideComponentProps) {
+  private _renderHoverButton() {
+    const {onPIPSwitch, playerHover} = this.props;
+    if (!playerHover) {
+      return null;
+    }
+    return (
+      <div className={styles.innerButtons}>
+        <div className={styles.iconContainer} onMouseUp={onPIPSwitch}>
+          <Icon id="dualscreen-side-by-side-pip" height={24} width={24} path={icons.SWITCH_TO_SIDE_BY_SIDE_ICON_PATH} />
+        </div>
+      </div>
+    );
+  }
+
+  render({guiClientRect}: SideBySideComponentProps) {
     const playerContainerStyles = {
       height: (guiClientRect?.width! / 2 / 16) * 9
     };
     return (
       <div ref={this.ref} className={styles.secondaryPlayer} style={playerContainerStyles}>
-        <div className={styles.innerButtons}>
-          <div className={styles.iconContainer} onMouseUp={onPIPSwitch}>
-            <Icon id="dualscreen-side-by-side-pip" height={24} width={24} path={icons.SWITCH_TO_SIDE_BY_SIDE_ICON_PATH} />
-          </div>
-        </div>
+        {this._renderHoverButton()}
       </div>
     );
   }
