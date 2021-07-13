21832ae94d70615f7df6e1d5a3306b42f134da26
Serhii Marchenko <serhii.marchenko@Kaltura.com>
2021-07-13 12:23:22 +0300

    fix(FEV-921): address PR comments

--- src/videoSyncManager.ts
+++ src/videoSyncManager.ts
@@ -71,11 +71,7 @@ export class VideoSyncManager {
     });
     this._eventManager.listen(this._mainPlayer, EventType.SEEKED, () => {
       this._logger.debug(`syncEvents :: seeked main player to ${this._mainPlayer.currentTime}`);
-      if (this._mainPlayer.paused) {
-        this._seekSecondaryPlayer(0);
-      } else {
-        this._secondaryPlayer.play();
-      }
+      this._seekSecondaryPlayer(0);
     });
     this._eventManager.listen(this._mainPlayer, EventType.ENDED, () => {
       this._logger.debug('syncEvents :: secondary player pause + seek to 0.01');
