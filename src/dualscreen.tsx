dcc8051c2388d29166811fc29da4cc1ba4ab2059
Roy Bregman <48884909+RoyBregman@users.noreply.github.com>
2021-10-04 11:38:01 +0300

    feat(FEV-993): getting thumb cue points using cue point service (#36)
    removing test temp code and registering to new cue point service

--- src/dualscreen.tsx
+++ src/dualscreen.tsx
@@ -84,6 +84,7 @@ export class DualScreen extends KalturaPlayer.core.BasePlugin implements IEngine
 
   loadMedia(): void {
     this._getSecondaryMedia();
+    this._getThumbs();
   }
 
   reset(): void {
@@ -387,6 +388,11 @@ export class DualScreen extends KalturaPlayer.core.BasePlugin implements IEngine
     }
   };
 
+  private _getThumbs() {
+    const kalturaCuePointService :any = this._player.getService('kalturaCuepoints');
+    kalturaCuePointService?.registerTypes([kalturaCuePointService.CuepointType.SLIDE]);
+  }
+
   private _getSecondaryMedia() {
     this._player.provider
       .doRequest([{loader: SecondaryMediaLoader, params: {parentEntryId: this._player.getMediaInfo().entryId}}])
@@ -412,9 +418,6 @@ export class DualScreen extends KalturaPlayer.core.BasePlugin implements IEngine
             this._secondaryPlayerType = PlayerType.IMAGE;
             this._imageSyncManager = new ImageSyncManager(this.eventManager, this.player, this._imagePlayer, this.logger);
             this._resolveReadyPromise();
-
-            // TEMP - for QA drop only
-            this.___addSlides();
           }
         }
       })
@@ -460,90 +463,4 @@ export class DualScreen extends KalturaPlayer.core.BasePlugin implements IEngine
     this.eventManager.destroy();
   }
 
-  // TEMP - for QA drop only
-  ___addSlides = () => {
-    const {RequestBuilder, ResponseTypes} = KalturaPlayer.providers;
-    class AssetLoader implements KalturaPlayerTypes.ILoader {
-      _parentEntryId: string = '';
-      _requests: any[] = [];
-      _response: any = {
-        assets: []
-      };
-      static get id(): string {
-        return 'dualscreen';
-      }
-      constructor(params: any) {
-        this._parentEntryId = params.parentEntryId;
-        const headers: Map<string, string> = new Map();
-        const request = new RequestBuilder(headers);
-        request.service = 'cuepoint_cuepoint';
-        request.action = 'list';
-        request.params = {
-          filter: {
-            objectType: 'KalturaThumbCuePointFilter',
-            entryIdEqual: this._parentEntryId,
-            cuePointTypeEqual: 'thumbCuePoint.Thumb',
-            subTypeIn: '1,2'
-          },
-          responseProfile: {
-            type: 1,
-            fields: 'id, assetId, startTime'
-          }
-        };
-        this.requests.push(request);
-      }
-
-      set requests(requests: any[]) {
-        this._requests = requests;
-      }
-      get requests(): any[] {
-        return this._requests;
-      }
-      set response(response: any) {
-        this._response.assets = response[0]?.data;
-      }
-      get response(): any {
-        return this._response;
-      }
-      isValid(): boolean {
-        return !!this._parentEntryId;
-      }
-    }
-
-    const ks = this._player.config.provider.ks;
-    const serviceUrl = this._player.config.provider.env.serviceUrl;
-
-    this._player.provider
-      .doRequest([{loader: AssetLoader, params: {parentEntryId: this._player.getMediaInfo().entryId}}])
-      .then((data: Map<string, any>) => {
-        if (data && data.has(SecondaryMediaLoader.id)) {
-          const assetLoader = data.get(SecondaryMediaLoader.id);
-          const assets = assetLoader?.response?.assets?.objects || [];
-          if (assets.length) {
-            let cuePoints = (assetLoader?.response?.assets?.objects || []).map((asset: any) => {
-              return {
-                assetUrl: `${serviceUrl}/index.php/service/thumbAsset/action/serve/thumbAssetId/${asset.assetId}/ks/${ks}?thumbParams:objectType=KalturaThumbParams&thumbParams:width=600`,
-                id: asset.id,
-                cuePointType: 'thumbCuePoint.Thumb',
-                startTime: asset.startTime / 1000
-              };
-            });
-            cuePoints = cuePoints.sort(function (a: any, b: any) {
-              return a.startTime - b.startTime;
-            });
-            cuePoints = cuePoints.map((cuePoint: any, index: number) => {
-              if (!cuePoint.endTime) {
-                return {...cuePoint, endTime: index === cuePoints.length - 1 ? Number.MAX_SAFE_INTEGER : cuePoints[index + 1].startTime};
-              }
-              return cuePoint;
-            });
-            console.log('>> cuePoints', cuePoints);
-            this._player.cuePointManager.addCuePoints(cuePoints);
-          }
-        }
-      })
-      .catch((e: any) => {
-        this.logger.error(e);
-      });
-  };
 }
