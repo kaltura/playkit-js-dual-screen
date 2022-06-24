0e95f12ea0feb6c4c0ebbe7049ede93c726a8fce
Serhii Marchenko <Serhii.marchenko@kaltura.com>
2022-06-24 11:12:05 +0300

    chore: revert upgrade dependencies

--- src/dualscreen.tsx
+++ src/dualscreen.tsx
@@ -96,6 +96,7 @@ export class DualScreen extends KalturaPlayer.core.BasePlugin implements IEngine
 
   reset(): void {
     this._setDefaultMode();
+    this._layout = Layout.Hidden;
     this._imagePlayer.reset();
     this._imageSyncManager?.reset();
     this._readyPromise = this._makeReadyPromise();
