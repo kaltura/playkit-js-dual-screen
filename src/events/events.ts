c1d033bab786e3ac31990540b93e979e4f77ba26
JonathanTGold <jonathan.gold@86@gmail.com>
2024-03-03 13:56:02 +0100

    [DualScreen Plugin]: Add Events (used by kava analytics)

---
+++ src/events/events.ts
@@ -0,0 +1,11 @@
+const namespace = 'dualscreen';
+
+/**
+ * dualscreen plugin event types.
+ */
+export const DualscreenEvents = {
+  /**
+   * Fired when dual-screen mode chances.
+   */
+  CHANGE_LAYOUT: `${namespace}-change_layout`,
+};
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 /// <reference path="./global.d.ts" />
 
 import {DualScreen} from './dualscreen';
+import {DualscreenEvents} from './events';
 
 declare var __VERSION__: string;
 declare var __NAME__: string;
@@ -10,6 +11,7 @@ const NAME = __NAME__;
 
 export {DualScreen as Plugin};
 export {VERSION, NAME};
+export {DualscreenEvents};
 
 const pluginName: string = 'dualscreen';
 KalturaPlayer.core.registerPlugin(pluginName, DualScreen);
