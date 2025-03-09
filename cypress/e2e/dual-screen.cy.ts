9e9b7066b8884795f45768f4a000a2e1d890dbd5
Roee Dean <roeedean@IL-ROEEDE-RND.lan>
2025-03-09 03:29:23 +0200

    FEC-14031-DUAL_SCREEN_CHANGE_LAYOUT-is-being-sent-upon-every-entry-change-in-playlist

--- cypress/e2e/dual-screen.cy.ts
+++ cypress/e2e/dual-screen.cy.ts
@@ -456,7 +456,7 @@ describe('Dual-Screen plugin', () => {
 
         cy.get('[data-testid="dualscreen_sideBySideWrapper"]').should('not.exist');
         cy.get('[data-testid="dualscreen_pipChildren"]').within(() => {
-          cy.get('[data-testid="dualscreen_switchToSideBySide"]').click({force: true})
+          cy.get('[data-testid="dualscreen_switchToSideBySide"]').click({force: true});
         });
         cy.get('[data-testid="dualscreen_sideBySideWrapper"]').should('exist').then(() => {
           // Manually dispatch the event
